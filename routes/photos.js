b4cbc692d7d46bacdf9390f25832bfc995dbd04b
Yoshiaki <yyyadachi0@gmail.com>
2021-01-15 20:18:21 +0900

    パラメータの表示を追加

--- routes/photos.js
+++ routes/photos.js
@@ -6,4 +6,12 @@ router.get('/', (req, res, next) => {
   res.send('Some photos');
 });
 
+// パラメーターの文字列を表示　XSS対応
+// res.writeHead はエラーとなりました。
+router.get('/:id', (req, res, next) => {
+  res.header('Content-Type', 'text/plain; charset=utf-8');
+  // res.render('photos', { title: 'PhotosPage', id: req.params.id });　うまく動かず。
+  res.send(`Some photos: ${req.params.id}`);
+});
+
 module.exports = router;
